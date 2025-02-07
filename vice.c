662335d31b578b478771abf8c2a4188055835ca8
TonyHawksBmx <dev729412@gmail.com>
2025-02-07 21:06:28 +0530

    Initial Commit

---
+++ vice.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+#include "defs.h"
+
+int main() {
+
+	AllInit();
+        
+        U64 playBitBoard = 0ULL;
+        
+        printf("Start:\n\n");
+        PrintBitBoard(playBitBoard);
+        
+        playBitBoard |= (1ULL<<SQ64(D2));
+        printf("D2 Added: \n\n");
+        PrintBitBoard(playBitBoard);
+        
+        playBitBoard |= (1ULL<<SQ64(G2));
+        printf("G2 Added: \n\n");
+        PrintBitBoard(playBitBoard);
+
+        int index = 0;
+
+	for ( index = 0; index < BRD_SQ_NUM; ++index) {
+                if (index%10==0) printf("\n");
+                printf("%5d",Sq120ToSq64[index]);
+        }
+        printf ("\n");
+        printf ("\n");
+        for (index = 0; index <64; ++index) {
+                if (index%8 == 0 ) printf("\n");
+                printf("%5d",Sq64ToSq120[index]);
+        }
+        printf ("\n");  //For Program Exit to look clean
+	return 0;
+}
